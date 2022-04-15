a5a4c89a339e05b8b138fea1a01a816307c87e76
Wiliel Florenciani <wiliel.florenciani@upr.edu>
2022-04-15 16:39:14 -0400

    updated progress bar to use a server sent event

--- src/StageArea/Stage.js
+++ src/StageArea/Stage.js
@@ -1,7 +1,6 @@
 import ProgressBar from "./ProgressBar";
 import { useParams } from "react-router-dom";
-import useFetchUpcomingEvents from "../hooks/use-fetch-upcoming-events";
-import useFetchServerSideEvents from "../hooks/use-fetch-server-side-events";
+import UpcomingEvents from "./UpcomingEvents";
 
 
 
@@ -11,36 +10,15 @@ const Stage = () => {
 
     // const {data: conf} = useFetch('http://localhost:8000/conference-info/'+ id); /* data is projects because info is found in db within projects */
 
-    var {events, isLoading} = useFetchUpcomingEvents();
-
-    function UpcomingEventsRender(props){
-        const p = props;
-        if(p.length>0){
-        return(
-            p.map((event) => {
-            return (
-            // Project list for schedule view in Lobby 
-            <div className="project-preview" key ={event.projectid}>
-                <h3>{event.starttime}</h3>
-            <h2>{event.title}</h2>
-            </div>
-            )
-            })
-        )
-        } else {
-        <h3>No events currently available</h3>
-        }
-    }
-
     return ( 
         <div>
             <br></br>
             <h2> STAGE </h2> 
             {/* {conf && ( 
                < div key={conf.id}> */}
-                <ProgressBar
-                    duration={25}
-                ></ProgressBar>
+               
+                <ProgressBar></ProgressBar>
+                
                 {/* </div> */}
             {/* )} */}
             <div className="bbb">
@@ -51,8 +29,9 @@ const Stage = () => {
                 <iframe className="iframe" src="https://iapstream.ece.uprm.edu/bigbluebutton/api/join?fullName=w1&meetingID=DemoMeeting&password=mp&role=moderator&checksum=62dcc9207e6fbaef56223b4f4b0dcd5abcad159e" allow="camera;microphone;display-capture" allowFullScreen></iframe> 
             </div>
             <div>
-                <div>events: {UpcomingEventsRender(events)}</div>
-                <div>{useFetchServerSideEvents()}</div>
+                {/* <div>{useFetchServerSentEvents()}</div> */}
+                <br></br>
+                <UpcomingEvents></UpcomingEvents>
             </div>
         </div>
 
