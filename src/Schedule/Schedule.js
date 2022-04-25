aae994f94d2dab3dfa2ee617fb040ec61492482e
johan017 <ashley.ortiz14@upr.edu>
2022-04-25 07:32:16 -0400

    Changed variable that displays the events
    added time factor to the display it self, changed to list instead of cal

--- src/Schedule/Schedule.js
+++ src/Schedule/Schedule.js
@@ -3,35 +3,79 @@ import { useState } from "react";
 import Calendar from './Calendar';
 import useFetch from "../useFetch";
 import { useHistory } from 'react-router-dom';
+import EventList from '../EventList';
+import useFetchEvents from "../hooks/use-fetch-events";
+
 
 
 const Schedule = () => {
-    const {id} = useParams();
-    const {data: schedule} = useFetch('http://localhost:8000/events/'); /* data is project because we want the id of a singular project */
-    const scheduleDone =0;
-
+    
     const history = useHistory();
+
+    const handelSchedule = () =>{
+        history.push('/cal');
+    }
 
     const handleEdit =() =>{
         history.push('/cal');
+    }
+    const {events,redirect, loading} = useFetchEvents();
+
+    const displayEvents = (props) => {
+        const e = props;
+        if(e.length>0){
+            // setScheduleDone(true);
+          return(
+            e.map((event) => {
+              return (
+              // event list for schedule view in Lobby 
+              <div className="schedule-prev" key ={event.projectid}>
+                    <p>{getTime(event.starttime)}</p>
+                  <h2>{event.title}</h2> 
+                  
+              </div>
+              )
+            })
+            )
+        } else {
+            // setScheduleDone(false);
+          <h3>No events currently available</h3>
+        }
+    }
+
+    const getTime = (props) =>{
+        const time = props;
+    
+        const eventDate = new Date(time);
+        var eventHours = eventDate.getHours();
+        const ampm = eventHours >=12? 'PM' : 'AM';
+        eventHours = (eventHours%12) || 12;
+        const eventMinutes = String(eventDate.getMinutes()).padStart(2, '0');
+        const eventTime = eventHours+":"+eventMinutes+" "+ampm;
+    
+        // var eventMinute =new Date(time).getMinutes;
+        console.log("hours", eventTime);
+        return(eventTime);
     }
 
     return ( 
         <div>   
             
-            {!schedule &&(
-                <Link to="/create_day">
-                    <button>Create Schedule</button>
-                </Link>
+            {!events &&(
+                // <Link to="/create_day">
+                    <button onClick={handelSchedule}>Create Schedule</button>
+                // </Link>
             )}
             {/* TODO ADD QUERY TO GET A LIST OF EVENTS FROM DB  */}
 
-            {schedule && (
-                <div> 
-                    <h2>Schedule Main</h2> 
+            {events && (
+                <div className="schedule-button"> 
+                    <h2>Schedule Main</h2>  <button onClick={handleEdit} >Edit Schedule</button>
+                    {displayEvents(events)}
 
-                    <Calendar/>
-                    <button onClick={handleEdit} >Edit Schedule</button>
+                    {/* <EventList></EventList> */}
+                    {/* <Calendar/> */}
+                   
                 </div>
 
             )}
