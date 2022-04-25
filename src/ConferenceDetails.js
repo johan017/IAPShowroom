780549eb12fcb76d91d030358024a6c4f99c1143
johan017 <ashley.ortiz14@upr.edu>
2022-04-25 12:30:44 -0400

    changes in: home, schedule, register, reset password

--- src/ConferenceDetails.js
+++ src/ConferenceDetails.js
@@ -1,59 +1,59 @@
-import { useHistory, useParams } from "react-router-dom";
-import useFetch from "./useFetch";
-import EventList from "./EventList";
-import { Link } from "react-router-dom";
-import C2 from './Schedule/C2';
+// import { useHistory, useParams } from "react-router-dom";
+// import useFetch from "./useFetch";
+// import EventList from "./EventList";
+// import { Link } from "react-router-dom";
+// import C2 from './Schedule/C2';
 
-const ConferenceDeatils = () => {
+// const ConferenceDeatils = () => {
 
-    const {id} = useParams();
-    const {data: conference, error, isLoading} = useFetch('http://localhost:8000/conference-info/' + id); /* data is project because we want the id of a singular project */
-    const history = useHistory();
-    const {data: events} = useFetch('http://localhost:8000/events'); /* data is events because info is found in db within events */
+//     const {id} = useParams();
+//     const {data: conference, error, isLoading} = useFetch('http://localhost:8000/conference-info/' + id); /* data is project because we want the id of a singular project */
+//     const history = useHistory();
+//     const {data: events} = useFetch('http://localhost:8000/events'); /* data is events because info is found in db within events */
 
 
-    const handleClick = () =>{
-        fetch('http://localhost:8000/conference-info/'+ conference.id, {
-            method: 'DELETE'
-        }).then(() => {
-            history.push('/schedule');
-        })
-    }
+//     const handleClick = () =>{
+//         fetch('http://localhost:8000/conference-info/'+ conference.id, {
+//             method: 'DELETE'
+//         }).then(() => {
+//             history.push('/schedule');
+//         })
+//     }
 
-    const handleSchedule = (e) =>{
-        e.preventDefault();
+//     const handleSchedule = (e) =>{
+//         e.preventDefault();
      
-        history.push('/create_schedule');
-    }
+//         history.push('/create_schedule');
+//     }
 
-    return (  
-        <div className = "event-details">
-            {isLoading && <div> Loading... </div>}
-            {error && <div> {error} </div>}
-            {conference && (
-                <div>
-                     {/* <h2>Calendar</h2> */}
+//     return (  
+//         <div className = "event-details">
+//             {isLoading && <div> Loading... </div>}
+//             {error && <div> {error} </div>}
+//             {conference && (
+//                 <div>
+//                      {/* <h2>Calendar</h2> */}
                      
-                    <h2>Conference Day {conference.day}</h2>
-                    <h2>Academic Session {conference.academicSession}</h2>
-                    <h2>Date:{conference.date}</h2>
-                    {/* <p> Start Time {event.startTime}</p>
-                    <p> End Time {event.endTime}</p>
-                    <div>Presented By: {event.presenters}</div> */}
-                    {/* {events && <EventList events={events} title="Events List"></EventList>} */}
-                    <Link to="/new_event">
-                        {!isLoading && <button>Add Event</button>} {/** adds the new event  */}
-                        {/* {isLoading && <button disabled>Adding Event...</button>} * add event button disabled while loading  */}
-                    </Link>
-                    {/* <C2/> */}
-                    <button onClick = {handleClick}>Delete</button>
-                    <button style={{ background: '#3B8D25' }} onClick = {handleSchedule}>Next</button>
+//                     <h2>Conference Day {conference.day}</h2>
+//                     <h2>Academic Session {conference.academicSession}</h2>
+//                     <h2>Date:{conference.date}</h2>
+//                     {/* <p> Start Time {event.startTime}</p>
+//                     <p> End Time {event.endTime}</p>
+//                     <div>Presented By: {event.presenters}</div> */}
+//                     {/* {events && <EventList events={events} title="Events List"></EventList>} */}
+//                     <Link to="/new_event">
+//                         {!isLoading && <button>Add Event</button>} {/** adds the new event  */}
+//                         {/* {isLoading && <button disabled>Adding Event...</button>} * add event button disabled while loading  */}
+//                     </Link>
+//                     {/* <C2/> */}
+//                     <button onClick = {handleClick}>Delete</button>
+//                     <button style={{ background: '#3B8D25' }} onClick = {handleSchedule}>Next</button>
 
-                </div>
-            )}
+//                 </div>
+//             )}
 
-        </div>
-    );
-}
+//         </div>
+//     );
+// }
  
-export default ConferenceDeatils;
+// export default ConferenceDeatils;
