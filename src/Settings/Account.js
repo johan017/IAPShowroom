d14add22264f44a81394d9be026e47b720a210af
johan017 <ashley.ortiz14@upr.edu>
2022-04-27 12:42:11 -0400

    added information of user accounts

--- src/Settings/Account.js
+++ src/Settings/Account.js
@@ -1,48 +1,62 @@
 import {Link} from "react-router-dom";
+import useFetchUserInfo from "../hooks/use-fetch-all-user-info";
 
 
 
 const Account = ({user_Role}) => {
+    const{userInfo} = useFetchUserInfo();
+
+    const getDate = (props) =>{
+        const today = new Date();
+        return today.toLocaleDateString('default', {month: 'long', day: 'numeric', year: 'numeric'});
+      }
+
     return ( 
         <div>
             <div className="account">
                 <h1>Account</h1>
 
                 <h2>Personal Information</h2>
-                    {/* <label>First Name: </label> <label>{first_name}</label>
-                    <label>Last Name: </label> <label>{last_name}</label>
-                    <label>Email: </label> <label>{email}</label>
-                    <label>Password: </label> <label>{password}</label>
-                    <label>Gender: </label> <label>{gender}</label> */}
-                    <label>Role: </label> <label>{user_Role}</label>
+                    <label>First Name: </label> <label>{userInfo.first_name}</label> <br/>
+                    <label>Last Name: </label> <label>{userInfo.last_name}</label> <br/>
+                    <label>Email: </label> <label>{userInfo.email}</label> <br/>
+                    {/* <label>Password: </label> <label>{userInfo.password}</label> <br/> */}
+                    <label>Gender: </label> <label>{userInfo.gender}</label> <br/>
+                    <label>Role: </label> <label>{userInfo.user_role}</label> <br/>
 
-                    {/* {user_Role === "Student Researcher" && (
+                    {userInfo.user_role === "Student Researcher" && (
                         <div>
-                        <label>Research Project: </label> <label>{projectTitles}</label>
-                        <label>Department: </label> <label>{department}</label>
-                        <label>Graduation Date: </label> <label>{grad_date}</label>
-                        <label>Project Manager: </label> <label>{ispm}</label>
+                        {/* <label>Research Project: </label> <label>{userInfo.projectTitles}</label> <br/>*/}
+                        <label>Major: </label> <label>{userInfo.department}</label> <br/>
+                        <label>Graduation Date: </label> <label>{getDate(userInfo.grad_date)}</label> <br/>
+                        {/* <label>Project Manager: </label> <label>{userInfo.ispm}</label> <br/>*/}
                         </div>
                     )}
 
-                    {user_Role === "Advisor" && (  
+                    {userInfo.user_role === "Advisor" && (  
                         <div>
-                        <label>Research Project: </label> <label>{projectTitles}</label>
+                        <label>Research Project: </label> <label>{userInfo.projectTitles}</label> <br/>
                         </div>
                     )}
 
-                    {user_Role === "Company Representative" && (  
+                    {userInfo.user_role === "Company Representative" && (  
                         <div> 
-                            <label>Company you Represent: </label> <label>{company_name}</label> 
+                            <label>Company you Represent: </label> <label>{userInfo.company_name}</label> <br/> 
                         </div>
-                    )} */}
+                    )}
 
                 <h2>Password Change</h2>
-                    
                 <p>Do you need to change your password? <Link to="/askChangePassword">Change Password</Link></p>
+               
                 <h2>Account Validation</h2>
-                <p>Do you need to receive an account validation email? <Link to ="/validate" >Validate your Account</Link></p>
 
+                {userInfo.verifiedemail === false &&(
+                <p>Do you need to receive an account validation email? <Link to ="/validate" >Validate your Account</Link></p>
+                )}
+
+                {userInfo.verifiedemail === true &&(
+                <p style={{color: "green", fontWeight:"bold"}}>You are a verified User !</p>
+                )}
 
 
 
