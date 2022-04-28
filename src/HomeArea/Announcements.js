9da0a3f5761cc72d5d84051e143f156ee7f8c532
johan017 <ashley.ortiz14@upr.edu>
2022-04-27 23:03:42 -0400

    gets and posts announcements to DB

--- src/HomeArea/Announcements.js
+++ src/HomeArea/Announcements.js
@@ -14,8 +14,8 @@ import useFetchAnnouncements from "../hooks/use-fetch-announcements";
 const Announcements = ({user_Role}) => {
 
     const {id} = useParams();
-    const {data: announcement} = useFetch('http://localhost:8000/announcements');
-    // const {announcements, redirect, isLoading } = useFetchAnnouncements();
+    // const {data: announcement} = useFetch('http://localhost:8000/announcements');
+    const {announcements, redirect, isLoading } = useFetchAnnouncements();
 
     const [currentMess, setCurrentMess] = useState('');
    
@@ -52,20 +52,20 @@ const Announcements = ({user_Role}) => {
         setCurrentMess('');
     }
 
-    // const getTime = (props) =>{
-    //     const time = props;
+    const getTime = (props) =>{
+        const time = props;
     
-    //     const eventDate = new Date(time);
-    //     var eventHours = eventDate.getHours();
-    //     const ampm = eventHours >=12? 'PM' : 'AM';
-    //     eventHours = (eventHours%12) || 12;
-    //     const eventMinutes = String(eventDate.getMinutes()).padStart(2, '0');
-    //     const eventTime = eventHours+":"+eventMinutes+" "+ampm;
+        const eventDate = new Date(time);
+        var eventHours = eventDate.getHours();
+        const ampm = eventHours >=12? 'PM' : 'AM';
+        eventHours = (eventHours%12) || 12;
+        const eventMinutes = String(eventDate.getMinutes()).padStart(2, '0');
+        const eventTime = eventHours+":"+eventMinutes+" "+ampm;
     
-    //     // var eventMinute =new Date(time).getMinutes;
-    //     console.log("hours", eventTime);
-    //     return(eventTime);
-    // }
+        // var eventMinute =new Date(time).getMinutes;
+        console.log("hours", eventTime);
+        return(eventTime);
+    }
 
 
     // const receiveAnnouncement = () =>{
@@ -96,14 +96,14 @@ const Announcements = ({user_Role}) => {
 
             <div className="admin-ann"> 
                 {/* <ScrollToBottom> */}
-                    {announcement && announcement.map((announce) => (
+                    {announcements && announcements.map((announce) => (
                         <div className="announce-body" key={announce.announcementid}>
-                            {/* <p>{announce.a_content}</p><br/> */}
-                            <p>{announce.message}</p><br/>
+                            <p>{announce.a_content}</p><br/>
+                            {/* <p>{announce.message}</p><br/> */}
 
-                            <text>{announce.time}</text>
+                            {/* <text>{announce.time}</text> */}
 
-                            {/* <text>{getTime(announce.a_date)}</text> */}
+                            <text>{getTime(announce.a_date)}</text>
                                         {/* {receiveAnnouncement()} */}
                         </div>
                     ))}
