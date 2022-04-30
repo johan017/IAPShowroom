1a0a494c1a0352bd53f3c7d309b038cae68dde46
johan017 <ashley.ortiz14@upr.edu>
2022-04-29 23:56:30 -0400

    added project members and advisors to proj room

--- src/RoomArea/ProjectRoom.js
+++ src/RoomArea/ProjectRoom.js
@@ -5,6 +5,8 @@ import { useHistory } from "react-router-dom";
 import { useState } from "react";
 import useFetchProjects from "../hooks/use-fetch-projects";
 import useFetchEvents from "../hooks/use-fetch-events";
+import axios from "../context/axios";
+
 
 // TODO: figure out why info doesnt appear with fetch & ids 
 const ProjectRoom = ({checked}) => {
@@ -15,6 +17,8 @@ const ProjectRoom = ({checked}) => {
     // const {data: event, error, isLoading} = useFetch('http://localhost:8000/projects/' + id); /* data is project because we want the id of a singular project */
     const {projects, isLoading} = useFetchProjects();
     const {events, loading} = useFetchEvents();   
+    console.log("events", events)
+    // console.log("event id", event)
     const [popup, setPopup] = useState(false);
    
 
@@ -32,6 +36,22 @@ const ProjectRoom = ({checked}) => {
     var  pathArray = window.location.pathname.split('/');
 
     var eid = parseInt(pathArray[2]);
+    const [roomInfo, setRoomInfo] = useState('');
+
+
+    const getSpeakers = async(pID) => {
+          try{
+          const result = await axios.get(`api/showroom/qna/info/1?meeting_id=${pID}`, 
+          {
+              headers: {"Content-Type": "application/json"},
+              withCredentials: true
+          }) 
+          setRoomInfo(result.data.payload.project_members);
+        //   setModalTitle(title)
+          } catch(error) {
+              console.error(error.response.status);
+          }
+      }
 
     const getProject = (e) =>{
         var inputArr = new Array();
@@ -59,44 +79,64 @@ const ProjectRoom = ({checked}) => {
         <div className = "project-room">
             {loading && <div> Loading... </div>}
             {/* {error && <div> {error} </div>} */}
+           
             {events && events.map((event) =>(
                 <div key={event.meetid}>                 
 
                     {event.projectid === eid &&(
                         <div>
-                            {/* <div style={{marginLeft: "1000px"}}>
-                            {checked && (<button onClick={handleStage} style={{backgroundColor: 'red'}}>STAGE LIVE</button>)}
-                            {!checked && (<button onClick={handleStage}>STAGE LIVE</button>)}   
-                            </div> */}
-                            {/* nArr = getProject(eid) */}
+                        
                             <div style={{marginLeft: "10px"}}>
-                                <button onClick={changePopup}>Project Information</button>  
+                                <button onClick={()=>{changePopup(); getSpeakers(event.projectid)}}>Project Information</button>  
                             </div>
                             
-                            
                             {popup == true && (
-                                <div>
+                                <div >
                                     {/* <h2>{event.title}</h2>
                                     <h3>{event.abstract}</h3> <br/> */}
-                                      <h2>{nArr[0]}</h2>
-                                    <h3>{nArr[2]}</h3> <br/>
-                                  
+                                      <h2>{nArr[0]}</h2> <br/>
+                                      <h3>Team Members</h3>
+                                        <div >
+                                            <h4> Student Researcher </h4>
+                                            {roomInfo && roomInfo.map((member)=> ( 
+                                                <>
+                                                {member.user_role === "Student Researcher" && (
+                                                <li>{member.first_name} {member.last_name}</li>
+                                                )}
+                                                </>
+                                            ))} 
+                                        </div>
+                                        {/* <br></br> */}
+                                        <div className="advisor-room-info">
+                                            <h4> Advisors </h4>
+                                            {roomInfo && roomInfo.map((member)=> ( 
+                                                <>
+                                                {member.user_role === "Advisor" && (
+                                                <li>{member.first_name} {member.last_name}</li>
+                                                )}
+                                                </>
+                                            ))} 
+                                        </div>
+                                      {/* {getSpeakers(event.projectid)} */}
+                                    <h3>Abstract</h3> <br/> <p>{nArr[2]}</p>                                  
                                 </div>
                             )}
 
 
                             <div className="bbb">
                                 {/* Update to get src url from the backend. Temporarily Hardcoded to get a view working  */}
-                                <iframe className="temp" src="https://iapstream.ece.uprm.edu/bigbluebutton/api/create?name=DemoMeeting&meetingID=DemoMeeting&attendeePW=ap&moderatorPW=mp&checksum=f5e85d6b55189f228cf06e4791736e44b63282f1"></iframe> {/*TODO Remove after changes to incorporate backend url*/}
+                                <iframe className="temp" src="https://iapstream.ece.uprm.edu/bigbluebutton/api/create?name=DemoMeeting&meetingID=DemoMeeting&attendeePW=ap&moderatorPW=mp&checksum=f5e85d6b55189f228cf06e4791736e44b63282f1"></iframe> 
                                 <br></br>
                                 <iframe className="iframe" src="https://iapstream.ece.uprm.edu/bigbluebutton/api/join?fullName=w1&meetingID=DemoMeeting&password=mp&role=moderator&checksum=62dcc9207e6fbaef56223b4f4b0dcd5abcad159e" allow="camera;microphone;display-capture" allowFullScreen></iframe>  
                             </div>
+                            
                         </div>
                     )} 
 
 
                 </div>
-            ))} 
+            ))}
+
             <Link to ="/rooms">
                 <button> Back to Rooms</button>
             </Link>
