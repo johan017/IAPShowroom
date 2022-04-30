1b074f408b8a545ed066d0a244bc75145939ccd7
johan017 <ashley.ortiz14@upr.edu>
2022-04-30 00:05:16 -0400

    added date formater for input default value starttime
    added function for posting a event not existing in table
    
    changed value used for projectID in handleUpdateEvent func
    
    cleaned the form area

--- src/Schedule/ScheduleUpdateEvent.js
+++ src/Schedule/ScheduleUpdateEvent.js
@@ -6,6 +6,7 @@ import Calendar from './Calendar';
 import axios from "../context/axios";
 import useFetchUserInfo from "../hooks/use-fetch-all-user-info";
 import { RestoreOutlined } from "@material-ui/icons";
+import useFetchEvents from "../hooks/use-fetch-events";
 
 
 
@@ -20,6 +21,7 @@ function ScheduleUpdateEvent (props) {
 
     const {userInfo} = useFetchUserInfo();
 
+    const {events} = useFetchEvents();
     
     const {projects} = useFetchProjects();
     var  pathArray = window.location.pathname.split('/');
@@ -39,7 +41,7 @@ function ScheduleUpdateEvent (props) {
     const [defaultST, setDefaultST] = useState();
 
 //    console.log(new Date(event.starttime))
-console.log (event)
+// console.log (event)
 // console.log(event.isdeleted)
 
     const getEvents = async() =>{
@@ -75,11 +77,6 @@ console.log (event)
 //     console.log("event starttime", new Date(event.starttime).toLocaleString('en-US'));
 //     console.log("event starttime", new Date(event.starttime).toLocaleString([], {month:'2-digit', day: '2-digit', year: 'numeric'}));
 
-
-
-
-
-
     const formatDate = (date) =>{
        const splitDate = new Date(date); //.split('T');
         const splitDate2 = date.split('T');
@@ -91,6 +88,18 @@ console.log (event)
         return `${sdate}`+" "+`${stime}`;
        
     }
+    // to display in modify date box 
+    const formatDate1 = (date) =>{
+        const splitDate = new Date(date); //.split('T');
+         const splitDate2 = date.split(' ');
+         const sdate = splitDate2[0].toString();
+         // console.log("date", sdate)
+         const stime = splitDate.toLocaleTimeString('it-IT'); //splitDate[1].toString()+":00";
+         // console.log("time", stime)
+ 
+         return `${sdate}`+"T"+`${stime}`;
+        
+     }
     const formatDate2 = (date) =>{
         const splitDate = date.split('T');
         const sdate = splitDate[0].toString();
@@ -100,27 +109,47 @@ console.log (event)
 
         return `${sdate}`+" "+`${stime}`;
     }
-    // console.log("event starttime ffn", formatDate(event.starttime));
 
+    {/* if an event does not exist - creat event [post] */}
+    const handleProjToEvent = async () =>{
+        var messageData = {
+            "adminid": userInfo.adminid,
+            "projectid": projectID, //parseInt(projectID),
+            "starttime": formatDate(startTime), //new Date(endTime).toLocaleString(),
+            "duration": parseInt(duration),
+            "title": title,
+            "e_date": formatDate2(startTime), //new Date(startTime), //.toLocaleString('en-US'),
+        };
+        console.log("event", messageData);
 
-    // console.log("event starttime formatFN", formatDate(startTime))
+        try{
 
-    console.log ("projects", projects)
+        await axios.post(`api/showroom/schedule/events/${eid}`, messageData, {
+            headers: {"Content-Type": "application/json"},
+            withCredentials: true
+            }).then((res) => {
+                console.log(res.data)
+            }).catch((error)=>{
+                console.log(error)
+        });
+        }catch(err){
 
-    const handleEvent = async () =>{ 
+        }
+    }
+    
+    {/* if an event already exists - update [put] */}
+    const handleUpdateEvent = async () =>{ 
         console.log("start time", startTime)
         var messageData = {
-                // "meetid": "",
                 "adminid": userInfo.adminid,
-                "projectid": parseInt(projectID),
+                "projectid": projectID, //parseInt(projectID),
                 "starttime": formatDate(startTime), //new Date(endTime).toLocaleString(),
                 "duration": parseInt(duration),
                 "title": title,
                 "e_date": formatDate2(startTime), //new Date(startTime), //.toLocaleString('en-US'),
-                // "isdeleted": false,
-            };
-            console.log("event", messageData);
-        // }
+        };
+        console.log("event", messageData);
+
         try{
 
         await axios.put(`api/showroom/schedule/events/${eid}`, messageData, {
@@ -137,7 +166,7 @@ console.log (event)
        
     }
 
- const handleDelete = async () =>{ 
+    const handleDelete = async () =>{ 
         console.log("Item deleted")
         await axios.delete(`api/showroom/schedule/events/${eid}`,  {
             headers: {"Content-Type": "application/json"},
@@ -147,122 +176,103 @@ console.log (event)
             }).catch((error)=>{
                 console.log(error)
         });
-        history.push('/cal');
-      
+        history.push('/cal'); 
     }
 
    
    
     return ( 
         <div className = "Event-information">
+            {/* if an event already exists - update [put] */}
             {event && (
-            //     <div>
-            // {event.isdeleted === false && (
-
-            <div className="addNewEvent">
-                {projects && projects.map((project)=>(
-                    <div key={project.project_id}>  
-                {/* // {events && ( */}
-                    {project.project_id === eid &&(
-                        <div>                        
-                        {event && (    
-                            <div>
-                         <h1>{new Date(event.starttime).toISOString('en-US').slice(0,16)}</h1>
-                         <h2>Event Information</h2>
-                         <label>Event Title: </label>
-                         <input 
-                             type="text" 
-                             defaultValue={project.title}
-                            //  value={title}
-                             onChange = {(e) => setTitle(e.target.value)}
-                         />
-                         <label>Start Time: </label>
-                         <input
-                             type="datetime-local"
-                             // required
-                             defaultValue={new Date(event.starttime).toISOString('en-US').slice(0,16)}
-                            //  value = {startTime}
-                             onChange = {(e) => setStartTime(e.target.value)}
-         
-                         ></input>
-                         <label>Duration: </label>
-                         <input 
-                             type="number" 
-                             // required 
-                             defaultValue={event.duration}
-                            //  value = {duration}
-                             onChange = {(e) => setDuration(e.target.value)}
-                         ></input>
-                     
-                         <Link to ={"/cal"}>
-                                 <button style={{ background: 'gray' }}>Cancel</button>
-                         </Link>
-                         <button style={{ background: '#3B8D25' }}  onClick={() => {handleEvent(); setProjectID(project.project_id); setTitle(project.title); }}>Update Event</button> 
-                         <button onClick={handleDelete}>Delete Event</button>
-                        </div>
-                    )}
-                {/* )} */}
-                    </div>
-                    )}
-                    </div>
-
-                ))}
-               
-            </div>
-            )}
-
-
-            {!event && (
-
-            <div className="addNewEvent">
-                {projects && projects.map((project)=>(
-                    <div key={project.project_id}>  
-                {/* // {events && ( */}
-                    {project.project_id === eid &&(
-                        <div>                        
-                            
-
-                        <h2>Event Information</h2>
-                        <label>Event Title: </label>
-                        <input 
-                            type="text" 
-                            defaultValue={project.title}
-                            //  value={title}
-                            onChange = {(e) => setTitle(e.target.value)}
-                        />
-                        <label>Start Time: </label>
-                        <input
-                            type="datetime-local"
-                            // required
-                            // defaultValue={new Date(defaultST).toISOString('en-US').slice(0,16)}
-                             value = {startTime}
-                            onChange = {(e) => setStartTime(e.target.value)}
-
-                        ></input>
-                        <label>Duration: </label>
-                        <input 
-                            type="number" 
-                            // required 
-                            // defaultValue={duration}
-                             value = {duration}
-                            onChange = {(e) => setDuration(e.target.value)}
-                        ></input>
+                <div className="addNewEvent">
+                    {events && events.map((uevent)=>(
+                        <div key={uevent.meetid}>  
+                            {uevent.meetid === eid &&(
+                                <div>                        
+                                    <h2>Event Information</h2>
+                                    <label>Event Title: </label>
+                                    <input 
+                                        type="text" 
+                                        defaultValue={uevent.title}
+                                        //  value={title}
+                                        onChange = {(e) => setTitle(e.target.value)}
+                                    />
+                                    <label>Start Time: </label>
+                                    <input
+                                        type="datetime-local"
+                                        // required
+                                        defaultValue={formatDate1(uevent.starttime)} 
+                                        //  value = {startTime}
+                                        onChange = {(e) => setStartTime(e.target.value)}
                     
-                        <Link to ={"/cal"}>
-                                <button style={{ background: 'gray' }}>Cancel</button>
-                        </Link>
-                        <button style={{ background: '#3B8D25' }}  onClick={() => {handleEvent(); setProjectID(project.project_id); setTitle(project.title); }}>Update Event</button> 
-                        <button onClick={handleDelete}>Delete Event</button>
+                                    ></input>
+                                    <label>Duration: </label>
+                                    <input 
+                                        type="number" 
+                                        // required 
+                                        defaultValue={uevent.duration}
+                                        //  value = {duration}
+                                        onChange = {(e) => setDuration(e.target.value)}
+                                    ></input>
+                                
+                                    <Link to ={"/cal"}>
+                                        <button style={{ background: 'gray' }}>Cancel</button>
+                                    </Link>
+                                    <button style={{ background: '#3B8D25' }}  onClick={() => {handleUpdateEvent(); setProjectID(parseInt(uevent.projectid)); setTitle(uevent.title); }}>Update Event</button> 
+                                    <button onClick={handleDelete}>Delete Event</button>
+                                </div>
+                            )}
                         </div>
-                    )}
-                {/* )} */}
-                    </div>
-
-                ))}
-            
-            </div>
+                    ))}             
+                </div>
             )}
-            {/* </div> */}
+
+            {/* if an event does not exist - creat event [post] */}
+            {!event && (
+                <div className="addNewEvent">
+                    {projects && projects.map((project)=>(
+                        <div key={project.project_id}>  
+                            {project.project_id === eid &&(
+                                <div>                        
+                                    <h2>Event Information</h2>
+                                    <label>Event Title: </label>
+                                    <input 
+                                        type="text" 
+                                        defaultValue={project.title}
+                                        //  value={title}
+                                        onChange = {(e) => setTitle(e.target.value)}
+                                    />
+                                    <label>Start Time: </label>
+                                    <input
+                                        type="datetime-local"
+                                        // required
+                                        // defaultValue={new Date(defaultST).toISOString('en-US').slice(0,16)}
+                                        value = {startTime}
+                                        onChange = {(e) => setStartTime(e.target.value)}
+
+                                    ></input>
+                                    <label>Duration: </label>
+                                    <input 
+                                        type="number" 
+                                        // required 
+                                        // defaultValue={duration}
+                                        value = {duration}
+                                        onChange = {(e) => setDuration(e.target.value)}
+                                    ></input>
+                                
+                                    <Link to ={"/cal"}>
+                                            <button style={{ background: 'gray' }}>Cancel</button>
+                                    </Link>
+                                    <button style={{ background: '#3B8D25' }}  onClick={() => {handleProjToEvent(); setProjectID(parseInt(project.project_id)); setTitle(project.title); }}>Update Event</button> 
+                                    <button onClick={handleDelete}>Delete Event</button>
+                                </div>
+                            )}
+                        </div>
+                    ))}
+                </div>
+            )}
+           
                 
 
             <div className="Event-information-calendar">
