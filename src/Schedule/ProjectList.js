09df13085f6ef8d6fd6f7b61ccafe3992d79e172
johan017 <ashley.ortiz14@upr.edu>
2022-04-30 00:00:12 -0400

    added new file for project list

---
+++ src/Schedule/ProjectList.js
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+// import useFetchEvents from "../hooks/use-fetch-events";
+// import useFetch from "./useFetch";
+import useFetchProjects from "../hooks/use-fetch-projects";
+
+
+//List of events inside the conference day 
+const ProjectList = () => {
+    const {projects, loading,} = useFetchProjects();
+    // const {id} = useParams();
+    //   const {data: events, isLoading} = useFetch('http://localhost:8000/events'); /* data is events because info is found in db within events */
+
+    return (
+
+        <div className="event-list">
+            {loading && <div> Loading...</div>}
+
+           {/* {displayEvents(events)} */}
+
+           {/* return(  */}
+            
+           {projects && projects.map((project) =>(
+                <div className="c2" key ={project.project_id}>
+                       {/* {project.projectid} */}
+                    <Link to ={`/event_details/${project.project_id}`}>
+                        {project.title}
+                        {/* <h2>Date {event.date}</h2> */}
+                    </Link> <br/>
+                    <Link to ={`/update_event/${project.project_id}`}>
+                        <button>Modify</button>
+                    </Link>                
+                </div>     
+                
+
+           ))}
+        
+        </div>
+    );
+}
+ 
+export default ProjectList;
