ad0e3770d25c240f858a194fc5dd208ee21b666b
johan017 <ashley.ortiz14@upr.edu>
2022-04-29 23:59:18 -0400

    cleaned document

--- src/Schedule/EventList.js
+++ src/Schedule/EventList.js
@@ -1,45 +1,14 @@
 import { Link } from "react-router-dom";
-// import useFetchEvents from "../hooks/use-fetch-events";
+import useFetchEvents from "../hooks/use-fetch-events";
 // import useFetch from "./useFetch";
-import useFetchProjects from "../hooks/use-fetch-projects";
+// import useFetchProjects from "../hooks/use-fetch-projects";
 
 
 //List of events inside the conference day 
 const EventList = () => {
-    const {projects, loading,} = useFetchProjects();
+    const {events, loading,} = useFetchEvents();
     // const {id} = useParams();
     //   const {data: events, isLoading} = useFetch('http://localhost:8000/events'); /* data is events because info is found in db within events */
-
-
-    // const displayEvents = (props) =>{
-    //     const e = props;
-    //     if(e.length>0){
-    //     // <h2>{title}</h2>
-    //         return( 
-            
-    //             e.map((event) =>{
-    //                 return(
-    //                     <div className="c2" key ={event.projectid}>
-                           
-    //                     <Link to ={`/event_details/${event.projectid}`}>
-    //                         {event.title}
-    //                         {/* <h2>Date {event.date}</h2> */}
-    //                     </Link>
-    //                     <Link to ={`/update_event/${event.projectid}`}>
-    //                         <button style={{ background: 'blue' }}>Modify</button>
-    //                     </Link>                
-    //                 </div>     
-    //                 )
-
-    //             })
-    //         )
-    //     }else{
-    //         <h3>No events currently available</h3>
-
-    //     }
-
-    // }
-    
 
     return (
 
@@ -50,14 +19,14 @@ const EventList = () => {
 
            {/* return(  */}
             
-           {projects && projects.map((project) =>(
-                <div className="c2" key ={project.project_id}>
+           {events && events.map((event) =>(
+                <div className="c2" key ={event.meetid}>
                        {/* {project.projectid} */}
-                    <Link to ={`/event_details/${project.project_id}`}>
-                        {project.title}
+                    <Link to ={`/event_details/${event.meetid}`}>
+                        {event.title}
                         {/* <h2>Date {event.date}</h2> */}
                     </Link> <br/>
-                    <Link to ={`/update_event/${project.project_id}`}>
+                    <Link to ={`/update_event/${event.meetid}`}>
                         <button>Modify</button>
                     </Link>                
                 </div>     
