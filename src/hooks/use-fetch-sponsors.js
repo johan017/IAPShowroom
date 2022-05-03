b6972763a2dd3b597bbfeb086a2dcc58b5dbab80
johan017 <ashley.ortiz14@upr.edu>
2022-05-03 00:26:01 -0400

    changed endpoint

--- src/hooks/use-fetch-sponsors.js
+++ src/hooks/use-fetch-sponsors.js
@@ -1,7 +1,7 @@
 import { useEffect, useState} from 'react';
 import axios from '../context/axios';
 
-const SPONSORS_URL = "/api/sponsors/"
+const SPONSORS_URL = "/api/showroom/sponsors/"
 
 const useFetchSponsors = () => {
 
