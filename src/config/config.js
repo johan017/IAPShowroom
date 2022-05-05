3cd47fb26e483303f994334b560b0c6032639f53
Wiliel Florenciani <wiliel.florenciani@upr.edu>
2022-05-04 22:20:27 -0400

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-const baseURL = 'http://localhost:8080';
-const WebSocketURL = 'ws://localhost:8081'
+const baseURL = 'https://iapshowroom.ece.uprm.edu';
+const WebSocketURL = 'wss://iapshowroom.ece.uprm.edu/ws/'
 
 const captchaKey = '6LeZSrUfAAAAACW7B4vSpseKslUZ-FzbLGKZpBla'
 
